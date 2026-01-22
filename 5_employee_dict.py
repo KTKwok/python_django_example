05331da0a2268079c3ecfb729b09ccafb020afac
KT <ktsteven@gmail.com>
2026-01-22 15:09:44 +0800

    Add files via upload

---
+++ 5_employee_dict.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    emp = {
+        "name": input("Please input employee name: "),
+        "age": int(input("Please input employee age: ")),
+        "gender": input("Please input employee gender: ")
+    }
+    print(emp)
